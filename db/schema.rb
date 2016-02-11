5d2217784ddbdcdac9a6d98ea51a79f74ac5062e
Ignat <bagaev.ignat@gmail.com>
2016-02-11 17:42:25 +0300

    new fields for client

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210154211) do
+ActiveRecord::Schema.define(version: 20160211140833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,17 @@ ActiveRecord::Schema.define(version: 20160210154211) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "avatar"
+    t.string   "sport"
+    t.string   "where"
+    t.string   "body"
+    t.integer  "up_size"
+    t.integer  "down_size"
+    t.integer  "foot_size"
+    t.text     "address"
+    t.string   "phone"
+    t.string   "city"
+    t.string   "country"
+    t.string   "zip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
