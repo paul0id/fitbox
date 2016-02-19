b93305f23e05b51c76c5327b3f04146dda44eff5
vova <Surgeon1989@gmail.com>
2016-02-19 16:09:09 +0300

    add friendly_id fix user creation

--- app/models/fit_box.rb
+++ app/models/fit_box.rb
@@ -1,7 +1,16 @@
 class FitBox < ActiveRecord::Base
+  extend FriendlyId
   belongs_to :manager, foreign_key: :manager_id
   belongs_to :client, foreign_key: :client_id
   has_and_belongs_to_many :items
+  friendly_id :slug
+
+  before_save :set_uuid
+
+  def set_uuid
+    self.slug = SecureRandom.uuid if self.slug.nil?
+  end
+
 
   enum status: {
     not_viewed: 0,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219104116) do
+ActiveRecord::Schema.define(version: 20160219121503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,11 @@ ActiveRecord::Schema.define(version: 20160219104116) do
     t.integer  "client_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
     t.integer  "status"
   end
+
+  add_index "fit_boxes", ["slug"], name: "index_fit_boxes_on_slug", using: :btree
 
   create_table "fit_boxes_items", id: false, force: :cascade do |t|
     t.integer "item_id"
@@ -33,6 +36,19 @@ ActiveRecord::Schema.define(version: 20160219104116) do
 
   add_index "fit_boxes_items", ["fit_box_id"], name: "index_fit_boxes_items_on_fit_box_id", using: :btree
   add_index "fit_boxes_items", ["item_id"], name: "index_fit_boxes_items_on_item_id", using: :btree
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
